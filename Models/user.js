736c9e39a7399afd884da528dec6db88f38ecd3c
Caleb Basil <brazilcalido@gmail.com>
2025-06-15 14:28:46 +0100

    user.js

---
+++ Models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: String,
+  address: String,
+  role: { type: String, default: 'user' },
+  likedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  resetToken: String,
+  resetTokenExpiry: Date,
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
